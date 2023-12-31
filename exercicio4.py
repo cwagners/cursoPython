265ba7a72a4ca0f7b91149fe648592badc911eef
Cristiano Wagner <cristiano.wagner@gmail.com>
2023-12-31 18:01:47 -0300

    aula 30/12/23 tarde

--- exercicio4.py
+++ exercicio4.py
@@ -1,10 +1,17 @@
 import beepy
 import time
 contagem = 10;
-for i in range(11):
+while contagem >= 0:
     print(contagem)
     contagem -= 1;
     time.sleep(1)
 
 beepy.beep(sound=1)
    
+number = int(input("Tabuada de: "))
+begin = int(input("De: "))
+end = int(input("Até: "))
+x = begin
+while x <= end:
+    print(f"{number} x {x} = {number * x}")
+    x = x + 1
